9d8b84ccea501807a8186ece7bd358970fe75275
ExoCode7 <bmg.achieve@gmail.com>
2025-02-17 01:13:19 -0500

    Make clear_everything.sh executable

---
+++ scripts/clear_everything.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Clearing Python code caches..."
+find . -type d -name "__pycache__" -exec rm -r {} +
+find . -type f -name "*.pyc" -delete
+find . -type f -name "*.pyo" -delete
+find . -type f -name "*.pyd" -delete
+rm -rf .pytest_cache
+
+echo "Clearing pre-commit cache..."
+pre-commit clean
+
+echo "Clearing conda cache..."
+conda clean --all --yes
+
+echo "Purging pip cache..."
+pip cache purge
+
+echo "All caches cleared!" 
